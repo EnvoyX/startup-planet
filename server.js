ab5579f699cbcb89a9a07f37026306d2d3a6d6ce
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-06-05 12:58:16 +0700

    feat : basic server setup

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Hello Server.js!");
+import express from "express";
+
+const PORT = 8000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
