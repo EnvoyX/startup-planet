0faf26f881d4ab877df52596270fa3d4b4541d1c
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-06-07 17:42:47 +0700

    feat : added error fallback

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const PORT = 8000;
 const app = express();
 
 app.use("/api", apiRouter);
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Endpoint not found. Please check the API documentation.",
+  });
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on port localhost: " + PORT);
